a174a6093fda45cee6df5639e6fa5db8c6109242
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2023-02-19 23:05:44 +0530

    day 1

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import  './Posts.css'
+import  './Posts.css';
+
+
 
 const Posts = () => {
   return (
     <div className='post'>
         <h3>UserName</h3>
-        <img src={'https://unsplash.com/photos/03sxrxp5V4k'} alt='post'/>
+        <img className='post__image' src='https://images.unsplash.com/photo-1611162616305-c69b3fa7fbe0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80://images.unsplash.com/photo-1676761998154-1153d04d82da?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80' alt='' />
+    
       {/* header -> avatar + userName */}
 
       {/* image */}
+      <h4 className='post__text'><strong>MrinalSingha</strong>: wow day 1 on building the website</h4>
 
       {/* UserName + Caption */}
     </div>
