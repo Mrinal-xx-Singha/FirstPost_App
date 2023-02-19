30eb34857a28492c18c3fc0332c514e9b6c92f44
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2023-02-19 22:15:04 +0530

    SlackPosts Build Day 1

---
+++ src/components/Posts.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import  './Posts.css'
+
+const Posts = () => {
+  return (
+    <div className='post'>
+        <h3>UserName</h3>
+        <img src={'https://unsplash.com/photos/03sxrxp5V4k'} alt='post'/>
+      {/* header -> avatar + userName */}
+
+      {/* image */}
+
+      {/* UserName + Caption */}
+    </div>
+  )
+}
+
+export default Posts
